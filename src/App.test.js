0e6e4e17306e6c1b98b8199d38050aeda9e131ec
Tony Gao <tonygao2k@hotmail.com>
2021-10-11 22:47:16 +0800

    1st Release

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders queryAll', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/查询全部/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders queryUsers', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/条件查询/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders modify', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/修改数据/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders deleteAll', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/清空测试数据/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
